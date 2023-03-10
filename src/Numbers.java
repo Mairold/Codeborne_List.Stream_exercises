dde6987a389c4e436b8bb07ad5297733aa4df688
tarkvarakool <tarkvarakool@epost.ee>
2023-03-10 15:56:11 +0200

    List excercise code refactored. Imperative and special methodes spliteed into different methods and tests. Easier to compare solutions

--- src/Numbers.java
+++ src/Numbers.java
@@ -1,15 +1,23 @@
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+import static java.util.Comparator.reverseOrder;
 
 public class Numbers {
 
-    public int sumOfElements(List<Integer> input) {
+    public int sumOfElementsImperative(List<Integer> input) {
         int sum = 0;
-//        for (Integer number : input) {
-//            sum += number;
-//        }
-//        return sum;
-        return input.stream().reduce(0, Integer::sum);
+        for (Integer number : input) {
+            sum += number;
+        }
+        return sum;
+    }
+
+    public int sumOfElementsSpecial(List<Integer> input) {
+        return input.stream().mapToInt(Integer::intValue).sum();
     }
 
     public List<Integer> concatenate(List<Integer> list1, List<Integer> list2) {
@@ -18,30 +26,48 @@ public class Numbers {
         return result;
     }
 
-    public List<Integer> reverse(List<Integer> integers) {
+    public List<Integer> reverseSpecial(List<Integer> integers) {
+        return IntStream.range(0, integers.size())
+                .boxed()
+                .sorted(reverseOrder())
+                .map(integers::get)
+                .collect(Collectors.toList());
+    }
+
+    public List<Integer> reverseImperative(List<Integer> integers) {
         List<Integer> reversed = new ArrayList<>();
         for (int i = integers.size() - 1; i >= 0; i--) {
             reversed.add(integers.get(i));
         }
-        //        Collections.reverse(reversed);
         return reversed;
     }
 
-    public List<Integer> sort(List<Integer> integers) {
+    public List<Integer> reverse(List<Integer> integers) {
+        List<Integer> reversed = new ArrayList<>(integers);
+        Collections.reverse(reversed);
+        return reversed;
+    }
+
+    public List<Integer> sortImperative(List<Integer> integers) {
         List<Integer> sortedList = new ArrayList<>(integers);
-//        boolean flag = true;
-//        while (flag) {
-//            flag = false;
-//            for (int i = 0; i < sortedList.size()-1; i++) {
-//                if (sortedList.get(i) > sortedList.get(i + 1)) {
-//                    int temp = sortedList.get(i);
-//                    sortedList.set(i, sortedList.get(i + 1));
-//                    sortedList.set(i + 1, temp);
-//                    flag = true;
-//                }
-//            }
-//        }
-//      return sortedList;
-        return sortedList.stream().sorted().toList();
+        System.out.println(sortedList);
+        boolean isNotFullySorted = true;
+        while (isNotFullySorted) {
+            isNotFullySorted = false;
+            for (int i = 0; i < sortedList.size() - 1; i++) {
+                if (sortedList.get(i) > sortedList.get(i + 1)) {
+                    int temp = sortedList.get(i);
+                    sortedList.set(i, sortedList.get(i + 1));
+                    sortedList.set(i + 1, temp);
+                    isNotFullySorted = true;
+                    System.out.println(sortedList);
+                }
+            }
+        }
+        return sortedList;
+    }
+
+    public List<Integer> sortSpecial(List<Integer> integers) {
+        return integers.stream().sorted().toList();
     }
 }
--- test/NumbersTest.java
+++ test/NumbersTest.java
@@ -1,46 +1,87 @@
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class NumbersTest {
 
     private final Numbers numbers = new Numbers();
 
     @Test
-    void sumOfElements() {
-        assertEquals(6, numbers.sumOfElements(List.of(1, 2, 3)));
+    void sumOfElements_Imperative() {
+        assertEquals(6, numbers.sumOfElementsImperative(List.of(1, 2, 3)));
     }
 
     @Test
-    void sumOfElements_minus() {
-        assertEquals(50, numbers.sumOfElements(List.of(-10, 100, -40)));
+    void sumOfElements_Imperative_negative() {
+        assertEquals(50, numbers.sumOfElementsImperative(List.of(-10, 100, -40)));
     }
 
     @Test
-    void sumOfElements_tyhi() {
-        assertEquals(0, numbers.sumOfElements(List.of()));
+    void sumOfElements_Imperative_empty() {
+        assertEquals(0, numbers.sumOfElementsImperative(List.of()));
+    }
+
+    @Test
+    void sumOfElements_Special() {
+        assertEquals(6, numbers.sumOfElementsSpecial(List.of(1, 2, 3)));
+    }
+
+    @Test
+    void sumOfElements_Special_negative() {
+        assertEquals(50, numbers.sumOfElementsSpecial(List.of(-10, 100, -40)));
+    }
+
+    @Test
+    void sumOfElements_Special_empty() {
+        assertEquals(0, numbers.sumOfElementsSpecial(List.of()));
     }
 
     @Test
     void concatenate() {
-        assertEquals(List.of(1,2,3,4,5,6,7,8), numbers.concatenate(List.of(1,2,3,4),List.of(5,6,7,8)));
+        assertEquals(List.of(1, 2, 3, 4, 5, 6, 7, 8), numbers.concatenate(List.of(1, 2, 3, 4), List.of(5, 6, 7, 8)));
     }
 
     @Test
     void reverse() {
-        assertEquals(List.of(1,2,3,4,5), numbers.reverse(List.of(5,4,3,2,1)));
+        ArrayList<Integer> original = new ArrayList<>(List.of(2, 3, 5, 4, 1));
+        assertEquals(List.of(1, 4, 5, 3, 2), numbers.reverse(original));
+        assertEquals(List.of(2, 3, 5, 4, 1), original);
     }
 
     @Test
-    void sort() {
-        assertEquals(List.of(1,2,3,4,5), numbers.sort(List.of(3,5,1,4,2)));
+    void reverseSpecial() {
+        ArrayList<Integer> original = new ArrayList<>(List.of(2, 3, 5, 4, 1));
+        assertEquals(List.of(1, 4, 5, 3, 2), numbers.reverseSpecial(original));
+        assertEquals(List.of(2, 3, 5, 4, 1), original);
     }
 
     @Test
-    void sort_biggerList() {
-        assertEquals(List.of(-5,-4,-3,-2,-1,0,1,2,3,4,5), numbers.sort(List.of(-1,3,-5,5,-4,1,0,4,-3,2,-2)));
+    void reverseImperative() {
+        ArrayList<Integer> original = new ArrayList<>(List.of(2, 3, 5, 4, 1));
+        assertEquals(List.of(1, 4, 5, 3, 2), numbers.reverseImperative(original));
+        assertEquals(List.of(2, 3, 5, 4, 1), original);
     }
 
+    @Test
+    void sort_Imperative() {
+        assertEquals(List.of(1, 2, 3, 4, 5), numbers.sortImperative(List.of(3, 5, 1, 4, 2)));
+    }
+
+    @Test
+    void sort_Imperative_biggerList() {
+        assertEquals(List.of(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5), numbers.sortImperative(List.of(-1, 3, -5, 5, -4, 1, 0, 4, -3, 2, -2)));
+    }
+
+    @Test
+    void sort_Special() {
+        assertEquals(List.of(1, 2, 3, 4, 5), numbers.sortSpecial(List.of(3, 5, 1, 4, 2)));
+    }
+
+    @Test
+    void sort_Special_BiggerList() {
+        assertEquals(List.of(-5, -4, -3, -2, -1, 0, 1, 2, 3, 4, 5), numbers.sortSpecial(List.of(-1, 3, -5, 5, -4, 1, 0, 4, -3, 2, -2)));
+    }
 }
