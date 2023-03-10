5ac7514ff19e444962e0ee71db713e100970f035
tarkvarakool <tarkvarakool@epost.ee>
2023-03-10 14:20:49 +0200

    List excercise task 2,3,4 completed

--- src/Numbers.java
+++ src/Numbers.java
@@ -5,9 +5,43 @@ public class Numbers {
 
     public int sumOfElements(List<Integer> input) {
         int sum = 0;
-        for (Integer number : input) {
-            sum += number;
+//        for (Integer number : input) {
+//            sum += number;
+//        }
+//        return sum;
+        return input.stream().reduce(0, Integer::sum);
+    }
+
+    public List<Integer> concatenate(List<Integer> list1, List<Integer> list2) {
+        ArrayList<Integer> result = new ArrayList<>(list1);
+        result.addAll(list2);
+        return result;
+    }
+
+    public List<Integer> reverse(List<Integer> integers) {
+        List<Integer> reversed = new ArrayList<>();
+        for (int i = integers.size() - 1; i >= 0; i--) {
+            reversed.add(integers.get(i));
         }
-        return sum;
+        //        Collections.reverse(reversed);
+        return reversed;
+    }
+
+    public List<Integer> sort(List<Integer> integers) {
+        List<Integer> sortedList = new ArrayList<>(integers);
+//        boolean flag = true;
+//        while (flag) {
+//            flag = false;
+//            for (int i = 0; i < sortedList.size()-1; i++) {
+//                if (sortedList.get(i) > sortedList.get(i + 1)) {
+//                    int temp = sortedList.get(i);
+//                    sortedList.set(i, sortedList.get(i + 1));
+//                    sortedList.set(i + 1, temp);
+//                    flag = true;
+//                }
+//            }
+//        }
+//      return sortedList;
+        return sortedList.stream().sorted().toList();
     }
 }
--- test/NumbersTest.java
+++ test/NumbersTest.java
@@ -22,4 +22,25 @@ class NumbersTest {
     void sumOfElements_tyhi() {
         assertEquals(0, numbers.sumOfElements(List.of()));
     }
+
+    @Test
+    void concatenate() {
+        assertEquals(List.of(1,2,3,4,5,6,7,8), numbers.concatenate(List.of(1,2,3,4),List.of(5,6,7,8)));
+    }
+
+    @Test
+    void reverse() {
+        assertEquals(List.of(1,2,3,4,5), numbers.reverse(List.of(5,4,3,2,1)));
+    }
+
+    @Test
+    void sort() {
+        assertEquals(List.of(1,2,3,4,5), numbers.sort(List.of(3,5,1,4,2)));
+    }
+
+    @Test
+    void sort_biggerList() {
+        assertEquals(List.of(-5,-4,-3,-2,-1,0,1,2,3,4,5), numbers.sort(List.of(-1,3,-5,5,-4,1,0,4,-3,2,-2)));
+    }
+
 }
